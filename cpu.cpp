3312fbe83d24bcd117d30cb07906ce0244eca648
Kerry Liu <kerryliu1016@gmail.com>
2023-12-01 12:44:42 -0800

    finished all types. debug later

--- cpu.cpp
+++ cpu.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 long int regfile[32] = {0};
+vector<uint32_t> memory(4068);
 int32_t pc = 0;
 
 /*
@@ -72,16 +73,230 @@ Instruction decode_stage(const string& binary_instruction){
 	return inst;
 }
 
-uint8_t lb(int rs1, int imm){
-	int memory_address = regfile[rs1] + imm;
-	return memory[memory_address] & 0xFF; //only want lower 8 bits
+void execute_instruction(const Instruction& decoded_inst, int32_t &pc, uint32_t base_addr){
+	unsigned int imm_bits = (decoded_inst.immediate >> 5) & 0x3F;
+	int imm_bits_signed = (decoded_inst.immediate >> 5) & 0x3F;
+			//Test Decode Output
+			//cout << "Executing Instruction:" << endl;
+			//cout << "Opcode: " << decoded_inst.opcode << endl;
+			//cout << "binary inst: " << binary_instruction << endl;
+			//cout << "pc: " << pc << endl;
+			//cout << "Func7: " << decoded_inst.func7 << endl;
+			//cout << "Func3: " << decoded_inst.func3 << endl;
+			//cout << "immediate: " << decoded_inst.immediate << endl;
+			//cout << "rs1: " << decoded_inst.rs1 << endl;
+			//cout << "rs2: " << decoded_inst.rs2 << endl;
+			//cout << "rd: " << decoded_inst.rd << endl;
+	if(decoded_inst.opcode == "0000011"){
+		cout << "Load (word only))" << endl;
+		uint32_t valid_address = base_addr + decoded_inst.immediate;
+		regfile[decoded_inst.rd] = load_word(valid_address);
+		cout << "Load: " << regfile[decoded_inst.rd] << "from address " << valid_address << endl;
+
+	}
+	else if(decoded_inst.opcode == "0010011"){
+		cout << "\tI-Type Instruction" << endl;
+		switch(decoded_inst.func3){
+			case 0x0: //addi
+				regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] + decoded_inst.immediate;
+				cout << "addi: " << decoded_inst.func3 << endl;
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+				break;
+			case 0x4: // xori
+				regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] ^ decoded_inst.immediate;
+				cout << "xori: " << decoded_inst.func3 << endl;
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+				break;
+			case 0x6: // ori
+				regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] | decoded_inst.immediate;
+				cout << "ori: " << decoded_inst.func3 << endl;
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+				break;
+			case 0x7: // andi
+				regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] & decoded_inst.immediate;
+				cout << "andi: " << decoded_inst.func3 << endl;
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+				break;
+			case 0x1: // slli
+				regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] << imm_bits;
+				cout << "slli: " << decoded_inst.func3 << endl;
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+				break;
+			case 0x5: // srli
+				// imm[5:11] = 0x00
+				if (decoded_inst.immediate == 010000){
+					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] >> imm_bits;
+					cout << "srli: " << decoded_inst.func3 << endl;
+					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+					cout << "PC: " << &pc << endl;
+				}
+				// imm[5:11] = 0x20 
+				else {
+					// srai
+					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] >> imm_bits_signed;
+					cout << "srai: " << decoded_inst.func3 << endl;
+					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+					cout << "PC: " << &pc << endl;
+				}
+				break;
+			case 0x2: //slti
+				if(regfile[decoded_inst.rs1] < decoded_inst.immediate){
+					regfile[decoded_inst.rd] = 1;
+				}
+				else {
+					regfile[decoded_inst.rd] = 0;
+				}
+				cout << "slti: " << decoded_inst.func3 << endl;
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+				break;	
+		}
+	}
+	else if(decoded_inst.opcode == "0110011"){
+		cout << "\tR-Type Instruction" << endl;
+		switch (decoded_inst.func3){
+			case 0x0:
+				switch(decoded_inst.func7){
+					case  0x00: //add
+						regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] + decoded_inst.rs2;
+						cout << "add: " << decoded_inst.func3 << endl;
+						cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+						cout << "PC: " << &pc << endl;
+						break;
+					case  0x20: //sub
+						regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] - decoded_inst.rs2;
+						cout << "sub: " << decoded_inst.func3 << endl;
+						cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+						cout << "PC: " << &pc << endl;
+						break;
+				}
+			case 0x4: //xor
+				regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] ^ decoded_inst.rs2;
+				cout << "xor: " << endl;
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+				break;
+			case 0x6: //or:
+				regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] | decoded_inst.rs2;
+				cout << "xor: " << endl;
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+				break;
+			case 0x7: //and:
+				regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] & decoded_inst.rs2;
+				cout << "and: " << endl;
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+				break;
+			case 0x1: //sll:
+				regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] << decoded_inst.rs2;
+				cout << "sll: " << endl;
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+				break;
+			case 0x5:
+				switch(decoded_inst.func7){
+					case 0x00: //srl
+						regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] >> decoded_inst.rs2;
+						cout << "srl: " << endl;
+						cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+						cout << "PC: " << &pc << endl;
+						break;
+					case 0x20: //sra
+						regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] >> decoded_inst.rs2_signed;
+						cout << "sra: " << endl;
+						cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+						cout << "PC: " << &pc << endl;
+						break;
+				}
+				break;
+			case 0x2: //slt
+				if(regfile[decoded_inst.rs1] < decoded_inst.rs2){
+					regfile[decoded_inst.rd] = 1;
+					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+					cout << "PC: " << &pc << endl;
+
+				}
+				else{
+					regfile[decoded_inst.rd] = 0;
+					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+					cout << "PC: " << &pc << endl;
+				}
+				break;
+			case 0x3: //sltu
+				if(regfile[decoded_inst.rs1] < decoded_inst.rs2){
+					regfile[decoded_inst.rd] = 1;
+					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+					cout << "PC: " << &pc << endl;
+				}
+				else{
+					regfile[decoded_inst.rd] = 0;
+					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+					cout << "PC: " << &pc << endl;
+				}
+				break;
+		}
+	}
+	else if(decoded_inst.opcode == "0100011"){
+		cout << "\tStore Instructions" << endl;
+		int memory_address = regfile[decoded_inst.rs1] + decoded_inst.immediate;
+		memory[memory_address] = regfile[decoded_inst.rs2];
+		cout << "Store instruction in address: " << memory_address << endl;
+	}
+	else if(decoded_inst.opcode == "1101111"){
+		cout << "J and B Instructions" << endl;
+		if(decoded_inst.func3 == "000"){ // TODO: Check BEQ opcode. is it the same as jump????
+			cout << "BEQ: " << endl;
+			if(regfile[decoded_inst.rs1] == regfile[decoded_inst.rs2]){
+				pc += decoded_inst.immediate;
+				cout << "PC: " << &pc << endl;
+			}
+		}
+		else {
+			cout << "JAL: " << endl;
+			long int jump_address = pc + decoded_inst.immediate - 1; // undo one pc
+			regfile[decoded_inst.rd] = pc + 1;
+			pc = jump_address;
+			cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+			cout << "PC: " << &pc << endl;
+		}
+	}
+	else if(decoded_inst.opcode == "0110111"){
+		cout << "LUI instruction " << endl;
+		regfile[decoded_inst.rd] = decoded_inst.immediate_lui << 12;
+		cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+		cout << "PC: " << &pc << endl;
+	}
+	else if(decoded_inst.opcode == "0010111"){
+		cout << "AUIPC instruction" << endl;
+		regfile[decoded_inst.rd] = pc + (decoded_inst.immediate_lui << 12);
+				cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
+				cout << "PC: " << &pc << endl;
+	}
+	pc++;
+
+}
+uint32_t load_word(uint32_t address){
+	if (address < memory.size()){
+		return memory[address];
+	}
+	else{
+		cout << "Memory" << address << "  out of range" << endl;
+		return 0;
+	}
 }
 
 int main(){
+	uint32_t baseAddress = 0x10010000; //set base address
+	vector<string> instr; // vector string for instructions
+
 	ifstream myfile;
 	string mystring;
-	vector<string> instr;
-	vector<uint8_t> memory(4096,0); //for load and store put in specific memory
 	myfile.open("r_type_cpp.dat");
 	if (myfile.is_open()){
 		while(getline(myfile, mystring)){
@@ -95,193 +310,33 @@ int main(){
 	}
 
 	
+	char user_input; 
+	cout << "Enter 'r': run entire program at once.  " << endl;
+	cout << "Enter 's': run one instruction at a time. Wait for next instruction. " << endl;
+
+	cin >> user_input;
+
+        if (user_input == 'r'){
 
 	// fetch the first instruction
-	while(pc < instr.size()){
-		string binary_instruction = instr[pc];
-		Instruction decoded_inst = decode_stage(binary_instruction);
-		
-		//Test Decode Output
-		//cout << "Executing Instruction:" << endl;
-        	//cout << "Opcode: " << decoded_inst.opcode << endl;
-		//cout << "binary inst: " << binary_instruction << endl;
-		//cout << "pc: " << pc << endl;
-        	//cout << "Func7: " << decoded_inst.func7 << endl;
-        	//cout << "Func3: " << decoded_inst.func3 << endl;
-        	//cout << "immediate: " << decoded_inst.immediate << endl;
-        	//cout << "rs1: " << decoded_inst.rs1 << endl;
-        	//cout << "rs2: " << decoded_inst.rs2 << endl;
-        	//cout << "rd: " << decoded_inst.rd << endl;
-		
-		unsigned int imm_bits = (decoded_inst.immediate >> 5) & 0x3F;
-		int imm_bits_signed = (decoded_inst.immediate >> 5) & 0x3F;
-
-		if(decoded_inst.opcode == "0010011"){
-			cout << "\tI-Type Instruction" << endl;
-			switch(decoded_inst.func3){
-				case 0x0: //addi
-					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] + decoded_inst.immediate;
-					cout << "addi: " << decoded_inst.func3 << endl;
-					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << &pc << endl;
-					break;
-				case 0x4: // xori
-					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] ^ decoded_inst.immediate;
-					cout << "xori: " << decoded_inst.func3 << endl;
-					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << &pc << endl;
-					break;
-				case 0x6: // ori
-					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] | decoded_inst.immediate;
-					cout << "ori: " << decoded_inst.func3 << endl;
-					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << &pc << endl;
-					break;
-				case 0x7: // andi
-					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] & decoded_inst.immediate;
-					cout << "andi: " << decoded_inst.func3 << endl;
-					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << &pc << endl;
-					break;
-				case 0x1: // slli
-					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] << imm_bits;
-					cout << "slli: " << decoded_inst.func3 << endl;
-					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << &pc << endl;
-					break;
-				case 0x5: // srli
-					// imm[5:11] = 0x00
-					if (decoded_inst.immediate == 010000){
-						regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] >> imm_bits;
-						cout << "srli: " << decoded_inst.func3 << endl;
-						cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-						cout << "PC: " << &pc << endl;
-					}
-					// imm[5:11] = 0x20 
-					else {
-						// srai
-						regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] >> imm_bits_signed;
-						cout << "srai: " << decoded_inst.func3 << endl;
-						cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-						cout << "PC: " << &pc << endl;
-					}
-					break;
-				case 0x2: //slti
-					if(regfile[decoded_inst.rs1] < decoded_inst.immediate){
-						regfile[decoded_inst.rd] = 1;
-					}
-					else {
-						regfile[decoded_inst.rd] = 0;
-					}
-					cout << "slti: " << decoded_inst.func3 << endl;
-					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << &pc << endl;
-					break;	
-			}
+		while(pc < instr.size()){
+			string binary_instruction = instr[pc];
+			Instruction decoded_inst = decode_stage(binary_instruction);
+			execute_instruction(decoded_inst, pc, baseAddress);
 		}
-		else if(decoded_inst.opcode == "0110011"){
-			cout << "\tR-Type Instruction" << endl;
-			switch (decoded_inst.func3){
-				case 0x0:
-					switch(decoded_inst.func7){
-						case  0x00: //add
-							regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] + decoded_inst.rs2;
-							cout << "add: " << decoded_inst.func3 << endl;
-							cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-							cout << "PC: " << &pc << endl;
-							break;
-						case  0x20: //sub
-							regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] - decoded_inst.rs2;
-							cout << "sub: " << decoded_inst.func3 << endl;
-							cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-							cout << "PC: " << &pc << endl;
-							break;
-					}
-				case 0x4: //xor
-					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] ^ decoded_inst.rs2;
-					cout << "xor: " << endl;
-					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << &pc << endl;
-					break;
-				case 0x6: //or:
-					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] | decoded_inst.rs2;
-					cout << "xor: " << endl;
-					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << &pc << endl;
-					break;
-				case 0x7: //and:
-					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] & decoded_inst.rs2;
-					cout << "and: " << endl;
-					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << &pc << endl;
-					break;
-				case 0x1: //sll:
-					regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] << decoded_inst.rs2;
-					cout << "sll: " << endl;
-					cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << &pc << endl;
-					break;
-				case 0x5:
-					switch(decoded_inst.func7){
-						case 0x00: //srl
-							regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] >> decoded_inst.rs2;
-							cout << "srl: " << endl;
-							cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-							cout << "PC: " << &pc << endl;
-							break;
-						case 0x20: //sra
-							regfile[decoded_inst.rd] = regfile[decoded_inst.rs1] >> decoded_inst.rs2_signed;
-							cout << "sra: " << endl;
-							cout << "\tResult: " << regfile[decoded_inst.rd] << endl;
-							cout << "PC: " << &pc << endl;
-							break;
-					}
-					break;
-				case 0x2: //slt
-					if(regfile[decoded_inst.rs1] < decoded_inst.rs2){
-						regfile[decoded_inst.rd] = 1;
-					}
-					else{
-						regfile[decoded_inst.rd] = 0;
-					}
-					break;
-				case 0x3: //sltu
-					if(regfile[decoded_inst.rs1] < decoded_inst.rs2){
-						regfile[decoded_inst.rd] = 1;
-					}
-					else{
-						regfile[decoded_inst.rd] = 0;
-					}
-					break;
-			}
-		}
-		else if(decoded_inst.opcode == "0000011"){
-			cout << "\tLoad Instructions" << endl;
-
-			switch(decoded_inst.func3){
-				case 0x0: //load byte
-					regfile[decoded_inst.rd] = lb(regfile[decoded_inst.rs1], decoded_inst.immediate);
-					cout << "LB" << endl;
-					cout << "Result: " << regfile[decoded_inst.rd] << endl;
-					cout << "PC: " << regfile[decoded_inst.rd] << endl;
-					break;
-				case 0x1: //load half
-					break;
-			}
-		}
-		else if(decoded_inst.opcode == "0100011"){
-			cout << "\tStore Instructions" << endl;
-		}
-		else if(decoded_inst.opcode == "1101111"){
-			cout << "J and B Instructions" << endl;
-		}
-		else if(decoded_inst.opcode == "0110111"){
-			cout << "LUI instruction " << endl;
-		}
-		else if(decoded_inst.opcode == "0010111"){
-			cout << "AUIPC instruction" << endl;
-		}
-		pc++;
+	}
+	else if(user_input == 's'){
+		string user_instruction;
+		cout << "Enter 32-bit instruction now: " << endl;
+		cin >> user_instruction;
+		string binary_instruction = user_instruction;
+		Instruction decoded_inst = decode_stage(binary_instruction)
+		execute_instruction(decoded_inst, pc, baseAddress);
+	}
+	else {
+		cout << "Invalid input" << endl;
+		return 1;
+	
 	}
 	return 0;
 }
